cd6b369df801288b5d7d90e7ef423aab5ae2f9da
Shenli Hartono <shenlihartono@gmail.com>
2020-04-25 19:55:38 +0700

    add command input parser

--- src/main/java/inc/park/App.java
+++ src/main/java/inc/park/App.java
@@ -1,7 +1,22 @@
 package inc.park;
 
+import inc.park.process.CommandParser;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Bandung");
+        CommandParser cmp = new CommandParser();
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            String s = scanner.nextLine();
+            if (s.equalsIgnoreCase("exit")) {
+                System.exit(0);
+            } else {
+                String result = cmp.process(s);
+                System.out.println(result);
+            }
+        } while (true);
     }
 }
