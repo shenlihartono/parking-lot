dd61dc619722b094e3d5fba33784a088c25d74dc
Shenli Hartono <shenlihartono@gmail.com>
2020-04-25 09:23:04 +0700

    add missing actions

--- src/main/java/inc/park/enums/ActionTypes.java
+++ src/main/java/inc/park/enums/ActionTypes.java
@@ -4,10 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum ActionTypes {
+    CREATE_PARKING_LOT("create_parking_lot"),
     PARK("park"), LEAVE("leave"), STATUS("status"),
     PLATE_NO_WITH_COLOR("registration_numbers_for_cars_with_colour"),
     SLOT_NUMBER_WITH_COLOR("slot_numbers_for_cars_with_colour"),
-    SLOT_NUMBER_WITH_PLATE("slot_number_for_registration_number");
+    SLOT_NUMBER_WITH_PLATE("slot_number_for_registration_number"),
+    EXIT("exit");
 
     private final String action;
     private static final Map<String, ActionTypes> actions = new HashMap<>();
