4394526cbed79eb7942da1f59d3cad0b92426ba3
Shenli Hartono <shenlihartono@gmail.com>
2020-04-25 10:41:16 +0700

    refactor code for createParkingLot to use constructor instead

--- src/main/java/inc/park/models/ParkingLot.java
+++ src/main/java/inc/park/models/ParkingLot.java
@@ -4,8 +4,13 @@ import java.util.Map;
 
 public class ParkingLot {
     private Map<Integer, ParkingSlot> parkSlots;
+    private final int size;
 
-    public String create(int size) {
+    public ParkingLot(int size) {
+        this.size = size;
+    }
+
+    public String create() {
         return "Created a parking lot with " + size + " slots";
     }
 }
--- src/test/java/inc/park/ParkingLotTests.java
+++ src/test/java/inc/park/ParkingLotTests.java
@@ -7,14 +7,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class ParkingLotTests {
-    private final ParkingLot parkingLot = new ParkingLot();
-
     @Test
     public void createParkingLot() {
-        String result = parkingLot.create(6);
+        ParkingLot parkingLot = new ParkingLot(6);
+        String result = parkingLot.create();
         assertThat(result, is("Created a parking lot with 6 slots"));
 
-        result = parkingLot.create(5);
+        parkingLot = new ParkingLot(5);
+        result = parkingLot.create();
         assertThat(result, is("Created a parking lot with 5 slots"));
     }
 }
